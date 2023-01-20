a3974a9daeb9fb60f1bc9ae3cce545dea984b1ab
Sellucoder <112238204+Sellucoder@users.noreply.github.com>
2023-01-20 18:04:08 +0530

    Add files via upload
    I have uploaded Practical of day 2&3{19/dec/23 & 20/dec/23}

---
+++ Practical/function.java
@@ -0,0 +1,14 @@
+class My{
+public static void Display()
+{
+System.out.println("apple mango");
+}
+}
+class function{
+public static void main(String args[])
+{
+System.out.println("hiii im at vashi ");
+My.Display();
+}
+}
+
